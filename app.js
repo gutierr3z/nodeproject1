fc78973a3f9a335f7a4627e8dbdea0fed8e749f2
Juan Gutierrez <gutierr3z@gmail.com>
2017-11-02 21:21:41 -0400

    practice

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
 var express = require( 'express' );
 var app = express();
 
+app.use( express.static( __dirname + '/public/' ) );
+
 app.get( '/', function( req, res ) {
     res.send( 'hi' );
 });
-
-app.use( express.static( __dirname ) );
 
 app.listen( 3000 );
 console.log( 'server running on 3000' );
